3592f84c19ae2b6f5b866a7ac973f2c66ac2d5b9
Mespyr <senukcw@gmail.com>
2023-07-23 14:37:15 -0400

    make fetch command send file so it bypasses the 2000 character limit

--- brony_tracker/client.py
+++ brony_tracker/client.py
@@ -1,4 +1,4 @@
-import discord, json
+import discord, json, io
 
 class Client(discord.Client):
     def __init__(self, data, data_path, *args, **kwargs):
@@ -33,36 +33,44 @@ class Client(discord.Client):
 
         command = message.content.split()
         if command[0] == "!fetch":
-            # not enough arguments were passed into command
-            if len(command) < 3:
-                await message.channel.send(f"Not enough arguments for `{command[0]}`.")
-                return
-            user = command[1]
-            channel = command[2]
-
-            # if the channel either doesn't exist or doesn't have any messages saved from it yet
-            if channel not in self.data["channels"]:
-                await message.channel.send(f"Channel `{channel}` either doesn't exist or doesn't have any messages saved yet.")
-                return
-
-            # fetch messages
-            await message.channel.send(f"Fetching messages sent in `{channel}` by `{user}`.")
-            channel_messages = self.data["channels"][channel]
-            msg_str = ""
-            for msg in channel_messages:
-                if msg[0] == user:
-                    msg_str += f'[{msg[3]} files | {msg[1]}] {msg[2]}\n'
-
-            if msg_str == "":
-                await message.channel.send(f"No messages found sent by `{user}`")
-                return
-            await message.channel.send(f"```{msg_str}```")
-
+            await self.fetch(command, message)
         elif command[0] == "!help":
-            await message.channel.send(f"""```
+            await self.help(message)
+        else:
+            await message.channel.send(f"Command `{command[0]}` does not exist.")
+
+    async def help(self, message):
+        await message.channel.send(f"""```
 !help               - display help message
 !fetch USER CHANNEL - fetch user's messages
 ```""")
 
-        else:
-            await message.channel.send(f"Command `{command[0]}` does not exist.")
+    async def fetch(self, command, message):
+        # not enough arguments were passed into command
+        if len(command) < 3:
+            await message.channel.send(f"Not enough arguments for `{command[0]}`.")
+            return
+        user = command[1]
+        channel = command[2]
+
+        # if the channel either doesn't exist or doesn't have any messages saved from it yet
+        if channel not in self.data["channels"]:
+            await message.channel.send(f"Channel `{channel}` either doesn't exist or doesn't have any messages saved yet.")
+            return
+
+        # fetch messages
+        channel_messages = self.data["channels"][channel]
+        data_str = ""
+        for msg in channel_messages:
+            if msg[0] == user:
+                data_str += f'[{msg[3]} files | {msg[1]}] {msg[2]}\n'
+
+        # if there is nothing found
+        if data_str == "":
+            await message.channel.send(f"No messages found sent by `{user}`")
+            return
+
+        byte_io = io.BytesIO(bytes(data_str, 'utf-8'))
+        f = discord.File(byte_io, filename = user + channel + ".txt")
+        
+        await message.channel.send(f"Fetched messages sent in `{channel}` by `{user}`.", file = f)
